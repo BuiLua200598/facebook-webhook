a296e744adb1b91b52a735089467468b2e1e2c7e
Duro <email-ban-dung-github@gmail.com>
2026-04-30 00:44:19 +0700

    fix backtick

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
-
+const axios = require("axios");
+const PAGE_ACCESS_TOKEN = "EAAcJ60iUEfYBRWCN0bZB13feWW5Pbpj7XCgL6GYHY1EZAI9DBqaLyuPS8ZBtfvJZA14N1dZB2B4HBMd5ZAd50UMyaW4Wd1HEMJtZByZBZAgpV7s2yFzoocc4cVix8es9mtDtQqEML134UovRmVib5SdTHNltIM60WoDUYVc10m8DjoIeFx8e9ydVwCg03XnNYICZAUe1orRfZBj8gZDZD";
+function sendMessage(sender_psid, text) {
+axios.post(
+  `https://graph.facebook.com/v18.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`
+  {
+    recipient: { id: sender_psid },
+    message: { text: text },
+  }
+);
+}
 app.use(express.json());
 
 const VERIFY_TOKEN = "Doankt@100299";
@@ -23,8 +33,28 @@ app.get("/webhook", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-  console.log("Facebook data:", JSON.stringify(req.body, null, 2));
-  res.sendStatus(200);
+  const body = req.body;
+
+  if (body.object === "page") {
+    body.entry.forEach(function (entry) {
+      entry.messaging.forEach(function (webhook_event) {
+
+        if (webhook_event.message) {
+          const sender_psid = webhook_event.sender.id;
+
+          sendMessage(
+            sender_psid,
+            "Chào bạn 👋 Áo ba lỗ đang sale 220K/2 áo 🔥 Bạn cần size gì?"
+          );
+        }
+
+      });
+    });
+
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
